49eead59fc6e1a20493fec92ff4547cbeca0a198
HannahSav <savongalina@yandex.ru>
2022-03-28 11:40:22 +0300

    Tests for Largests HeapSort

--- lab1/src/test/java/HeapSortTest.java
+++ lab1/src/test/java/HeapSortTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,6 +15,7 @@ public class HeapSortTest {
     private HeapSort heap = new HeapSort();
 
      @Test
+     @Tag("SortedTest")
      @DisplayName("Already sort one")
     public void alreadySortedTest() {
         double[] input = {1, 2, 3, 4, 5, 6, 7};
@@ -22,6 +25,7 @@ public class HeapSortTest {
     }
 
     @Test
+    @Tag("SortedTest")
     @DisplayName("Empty array")
     public void emptyArrayTest() {
         double[] input = {};
@@ -31,6 +35,7 @@ public class HeapSortTest {
     }
 
     @Test
+    @Tag("SortedTest")
     @DisplayName("Simple test")
     public void simpleTest() {
         double[] input = {2, 1, -3, -8, 9, 4};
@@ -40,6 +45,7 @@ public class HeapSortTest {
     }
 
     @Test
+    @Tag("SortedTest")
     @DisplayName("One element test")
     public void oneElemTest() {
         double[] input = {1};
@@ -49,6 +55,7 @@ public class HeapSortTest {
     }
 
     @Test
+    @Tag("SortedTest")
     @DisplayName("Big number test")
     public void bigNumberTest() {
         double[] input = {12134, Integer.MAX_VALUE, 8, Integer.MIN_VALUE, 0, 1, -6767};
@@ -58,6 +65,7 @@ public class HeapSortTest {
     }
 
     @Test
+    @Tag("SortedTest")
     @DisplayName("Double elements test")
     public void DoubleElemTest() {
         double[] input = {1.1, 1.5, 2.2, -1.1, 3.3};
@@ -66,6 +74,7 @@ public class HeapSortTest {
         assertArrayEquals(expected, input, 0.0);
     }
 
+    @Tag("LargestTest")
     @Test
     @DisplayName("Find Largest if it's parent")
     public void LargestTest() {
@@ -74,6 +83,7 @@ public class HeapSortTest {
     }
 
     @Test
+    @Tag("LargestTest")
     @DisplayName("Find Largest if it's first child")
     public void LargestChildTest() {
         double[] input = {3.77, 3.78, 3.76};
@@ -81,9 +91,26 @@ public class HeapSortTest {
     }
 
     @Test
+    @Tag("LargestTest")
     @DisplayName("Find Largest if it's second child")
     public void LargestSecondChildTest() {
         double[] input = {3.76, 3.78, 3.87};
         assertEquals(heap.findLargest(0, 3, 0, input), 2, 0.0);
+    }
+
+    @Test
+    @Tag("LargestTest")
+    @DisplayName("Find Largest if only one child")
+    public void OnlyOneChildTest() {
+        double[] input = {3.76, 3.78, 3.87, 4.78};
+        assertEquals(heap.findLargest(1, 4, 1, input), 3, 0.0);
+    }
+
+    @Test
+    @Tag("LargestTest")
+    @DisplayName("Find Largest with no child")
+    public void NoChildTest() {
+        double[] input = {3.76, 3.78, 3.87, 4.78};
+        assertEquals(heap.findLargest(3, 4, 3, input), 3, 0.0);
     }
 }
